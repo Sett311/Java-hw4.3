bdc162f8bc085f7b007c5ce65f59acc934631ce5
Sett311 <2004sett@gmail.com>
2023-05-17 21:22:20 +0300

    update formula

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,9 +2,8 @@ public class CreditPaymentService {
     public double calculate(int credit, int year) {
         short months = (short) (year * 12);
         float interestRate = 9.99F;
-        double i = Math.pow(1 + (interestRate /100 / 12), months);
-        double payment = Math.ceil(credit * (interestRate / 100 / 12 * i) / (i -1));
-        return payment;
+        double i = Math.pow(1 + (interestRate / 100 / 12), months);
+        return Math.ceil(credit * (interestRate / 100 / 12 * i) / (i - 1));
     }
 }
 // А=К*(П/(1+П)-М-1), где К – сумма кредита, П – процентная ставка, М – количество месяцев
