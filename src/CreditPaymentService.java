5c3c7c5772887af6b2da23a006fe46ded4f6b9c6
Sett311 <2004sett@gmail.com>
2023-05-17 07:55:05 +0300

    update gitignore

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class CreditPaymentService {
+public class CreditPaymentService {
+    public float calculate(int credit, int year) {
+        short months = (short) (year * 12);
+        float interestRate = 9.99F;
+        float i = interestRate /100 * 12;
+        double payment = credit * Math.pow(i, months) / (1 -  Math.pow(1 + i, -months));
+        return (float) payment;
+    }
 }
+// А=К*(П/(1+П)-М-1), где К – сумма кредита, П – процентная ставка, М – количество месяцев
+// Sе = S*P /12*(1+P/12)N/((1+P/12)N -1)
